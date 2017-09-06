c6b6f28228e3aa5e47b098813a7cb69bc4ff1e79
sfgjys <sfgjys@sina.com>
2017-09-06 10:28:30 +0800

    写文档

--- livelibrary/src/main/java/com/alivc/videochat/publisher/AudioPusher.java
+++ livelibrary/src/main/java/com/alivc/videochat/publisher/AudioPusher.java
@@ -6,21 +6,13 @@
 package com.alivc.videochat.publisher;
 
 import android.content.Context;
-import android.media.AudioManager;
 import android.media.AudioRecord;
-import android.media.AudioTrack;
 import android.media.audiofx.AcousticEchoCanceler;
-import android.os.Environment;
 import android.os.Process;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import com.alivc.videochat.utils.LogUtil;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.LinkedList;
 
 public class AudioPusher {
     private static final String TAG = "AudioPusher0927";
@@ -35,10 +27,6 @@ public class AudioPusher {
     private Context mContext;
     private byte[] mMuteData = null;
     private int mFrameSize;
-    private AudioTrack mPlayPCMTool;
-    private LinkedList<byte[]> mListByte;
-    private byte[] mPlayByte;
-    private boolean mThreadRoutineIsContinue;
 
     public AudioPusher(Context context) {
         LogUtil.d("AudioPusher0927", "new AudioPusher.");
@@ -68,23 +56,7 @@ public class AudioPusher {
                 this.minBufferSize = this.mFrameSize;
             }
 
-            // 参数一为MIC 参数二由setRecordParams方法设定（默认32000） 参数三双声道 参数四
             this.audioRecord = new AudioRecord(1, this.mSampleRateInHz, 12, 2, this.minBufferSize * 10);
-
-            // AudioTrack 得到播放最小缓冲区的大小
-            int mPlayMinBufferSize = AudioTrack.getMinBufferSize(this.mSampleRateInHz, 12, 2);
-            // 实例化播放音频对象
-            mPlayPCMTool = new AudioTrack(AudioManager.STREAM_MUSIC, this.mSampleRateInHz, 12, 2, mPlayMinBufferSize, AudioTrack.MODE_STREAM);
-
-            // 实例化一个链表，用来存放字节组数
-            mListByte = new LinkedList<>();
-
-            // --------------------------------------------------------------------------------------------------------
-
-            // 实例化一个长度为播放最小缓冲大小的字节数组
-            mPlayByte = new byte[mPlayMinBufferSize];
-
-
             this.mStatus = Status.RUNNING;
             this.mPusherRuning = true;
             if (this.audioRecord.getRecordingState() == 1) {
@@ -94,13 +66,10 @@ public class AudioPusher {
                     throw new PublisherException("audio record read fail");
                 }
 
-                mThreadRoutineIsContinue = true;
-
                 (new Thread(new AudioPusher.AudioRecordTask())).start();
-                (new Thread(new PlayPCM())).start();
             }
-
         }
+
     }
 
     public void pause() {
@@ -131,7 +100,6 @@ public class AudioPusher {
                 this.mPusherRuning = false;
                 if (this.audioRecord.getRecordingState() == 3) {
                     this.audioRecord.stop();
-                    mThreadRoutineIsContinue = false;
                 }
             }
 
@@ -140,8 +108,8 @@ public class AudioPusher {
                 this.mAEC.release();
                 this.mAEC = null;
             }
-
         }
+
     }
 
     public void release() {
@@ -154,6 +122,7 @@ public class AudioPusher {
 
             this.audioRecord = null;
         }
+
     }
 
     public void setAudioSourceListener(AudioPusher.AudioSourceListener listener) {
@@ -172,110 +141,47 @@ public class AudioPusher {
         return calling;
     }
 
+    public interface AudioSourceListener {
+        void onAudioFrame(byte[] var1, int var2);
+    }
+
     class AudioRecordTask implements Runnable {
         AudioRecordTask() {
         }
 
         public void run() {
-            Log.d("AudioPusher0927", "run begin ." + AudioPusher.this.mPusherRuning + " " + AudioPusher.this.audioRecord.getRecordingState());
+            Log.d("AudioPusher0927", "run begin ." + AudioPusher.this.mPusherRuning + " " +
+                    AudioPusher.this.audioRecord.getRecordingState());
 
             try {
                 Process.setThreadPriority(-19);
             } catch (Exception var8) {
                 Log.e("AudioPusher0927", "Set record thread priority failed: " + var8.getMessage());
             }
-            FileInputStream fileInputStreamFromFile = getFileInputStreamFromFile(Environment.getExternalStorageDirectory() + "/Decode/123456.pcm");
 
             byte[] buffer = new byte[AudioPusher.this.mFrameSize];
-            byte[] pcmBuffer = new byte[AudioPusher.this.mFrameSize];
-            byte[] collectMiddleBytes;
             long time = System.currentTimeMillis();
             long startTime = time;
             int count = 0;
 
             while (AudioPusher.this.mPusherRuning && AudioPusher.this.audioRecord.getRecordingState() == 3) {
-                try {
-                    int len = AudioPusher.this.audioRecord.read(buffer, 0, buffer.length);
-                    int read = fileInputStreamFromFile.read(pcmBuffer);
-                    ++count;
-                    Log.d("AudioPusher0927", "audio: on audio " + len + " time : " + (System.currentTimeMillis() - time) + "average time : " + (System.currentTimeMillis() - startTime) / (long) count);
-                    time = System.currentTimeMillis();
-                    if (0 < len && (AudioPusher.this.mStatus != Status.PAUSED || !AudioPusher.this.isTelephonyCalling())) {
-                        if (AudioPusher.this.mMute) {
-                            if (AudioPusher.this.mAudioSourceListener != null) {
-                                AudioPusher.this.mAudioSourceListener.onAudioFrame(AudioPusher.this.mMuteData, AudioPusher.this.mFrameSize);
-                            }
-                        } else if (AudioPusher.this.mAudioSourceListener != null) {
-                            //  测试一
-//                            if (read != -1) {
-//                                System.out.println("9527LEN: " + len);
-//                                AudioPusher.this.mAudioSourceListener.onAudioFrame(pcmBuffer, read);
-//                            } else {
-//                                AudioPusher.this.mAudioSourceListener.onAudioFrame(buffer, len);
-//                            }
-                            // 测试二
-//                            for (int i = 0; i < buffer.length; i++) {
-//                                buffer[i] = (byte) (buffer[i] * 1.8f);
-//                            }
-//                            AudioPusher.this.mAudioSourceListener.onAudioFrame(buffer, len);
-                            // 测试三
-                            if (read != -1) {
-                                for (int i = 0; i < buffer.length; i++) {
-                                    buffer[i] = (byte) (pcmBuffer[i] * 0.9f + buffer[i] * 1.8f);
-                                }
-                            }
-                            collectMiddleBytes = buffer.clone();
-                            if (mListByte.size() >= 2) {
-                                mListByte.removeFirst();
-                            }
-                            mListByte.add(collectMiddleBytes);
-                            AudioPusher.this.mAudioSourceListener.onAudioFrame(buffer, len);
+                int len = AudioPusher.this.audioRecord.read(buffer, 0, buffer.length);
+                ++count;
+                Log.d("AudioPusher0927", "audio: on audio " + len + " time : " + (System.currentTimeMillis() - time) + "averagetime : " + (System.currentTimeMillis() - startTime) / (long) count);
+                time = System.currentTimeMillis();
+                if (0 < len && (AudioPusher.this.mStatus != Status.PAUSED || !AudioPusher.this.isTelephonyCalling())) {
+                    if (AudioPusher.this.mMute) {
+                        if (AudioPusher.this.mAudioSourceListener != null) {
+                            AudioPusher.this.mAudioSourceListener.onAudioFrame(AudioPusher.this.mMuteData,
+                                    AudioPusher.this.mFrameSize);
                         }
+                    } else if (AudioPusher.this.mAudioSourceListener != null) {
+                        AudioPusher.this.mAudioSourceListener.onAudioFrame(buffer, len);
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             }
-            fileInputStreamFromFile = null;
 
             Log.e("AudioPusher0927", "exist debug.");
-        }
-    }
-
-    public interface AudioSourceListener {
-        void onAudioFrame(byte[] var1, int var2);
-    }
-
-    private static FileInputStream getFileInputStreamFromFile(String fileUrl) {
-        FileInputStream fileInputStream = null;
-
-        try {
-            File file = new File(fileUrl);
-
-            fileInputStream = new FileInputStream(file);
-        } catch (Exception e) {
-            System.out.println("GetBufferedInputStreamFromFile异常");
-        }
-
-        return fileInputStream;
-    }
-
-    private class PlayPCM implements Runnable {
-        @Override
-        public void run() {
-            byte[] playMiddle;
-            // 开始播放
-            mPlayPCMTool.play();
-
-            while (mThreadRoutineIsContinue) {
-                try {
-                    mPlayByte = mListByte.getFirst();
-                    playMiddle = mPlayByte.clone();
-                    mPlayPCMTool.write(playMiddle, 0, playMiddle.length);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
